cc258cc659e0faf25a0989170b3e6a26b082c41f
donia98 <nadooshah98@gmail.com>
2020-05-18 00:19:35 +0300

    fixed jdbc exceptions

--- JDBC/src/stdapp/TableViewPaneController.java
+++ JDBC/src/stdapp/TableViewPaneController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -55,7 +56,7 @@ public class TableViewPaneController implements Initializable {
     private Button buttonAddCourse;
     @FXML
     private Button buttonReset;
-    
+
     Statement statement;
     @FXML
     private Button buttonShow;
@@ -76,35 +77,49 @@ public class TableViewPaneController implements Initializable {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        tcID.setCellValueFactory(new PropertyValueFactory("stdId"));
-        tcName.setCellValueFactory(new PropertyValueFactory("stdName"));
-        tcMajor.setCellValueFactory(new PropertyValueFactory("stdMajor"));
-        tcGrade.setCellValueFactory(new PropertyValueFactory("stdGrade"));
+        tcID.setCellValueFactory(new PropertyValueFactory("id"));
+        tcName.setCellValueFactory(new PropertyValueFactory("name"));
+        tcMajor.setCellValueFactory(new PropertyValueFactory("major"));
+        tcGrade.setCellValueFactory(new PropertyValueFactory("grade"));
         tableView.getSelectionModel().selectedItemProperty().addListener(
                 event -> showSelectedStudent());
 
-    } 
-    
-        @FXML
-    private void buttonShowHandle(ActionEvent event) throws Exception{
+    }
+
+    @FXML
+    private void buttonShowHandle(ActionEvent event) throws Exception {
         ResultSet rs = this.statement.executeQuery("Select * From Student");
         tableView.getItems().clear();
-        while(rs.next()){
+        while (rs.next()) {
             Student std = new Student();
-            std.setStdId(rs.getInt("stdID"));
-            std.setStdName(rs.getString("stdName"));
-            std.setStdMajor(rs.getString("stdMajor"));
-            std.setStdGrade(rs.getDouble("stdGrade"));
+            std.setStdId(rs.getInt("id"));
+            std.setStdName(rs.getString("name"));
+            std.setStdMajor(rs.getString("major"));
+            std.setStdGrade(rs.getDouble("grade"));
             tableView.getItems().add(std);
         }
     }
 
     @FXML
-    private void buttonAddHandle(ActionEvent event) {
+    private void buttonAddHandle(ActionEvent event) throws SQLException {
+        Integer id = Integer.parseInt(stdID.getText());
+        String name = stdName.getText();
+        String major = stdMajor.getText();
+        Double grade = Double.parseDouble(stdGrade.getText());
+        String sql = "Insert Into Student values(" + id + ",'" + name + "','"
+                + major + "'," + grade + ")";
+        this.statement.executeUpdate(sql);
     }
 
     @FXML
-    private void buttonUpdateHandle(ActionEvent event) {
+    private void buttonUpdateHandle(ActionEvent event) throws SQLException {
+                Integer id = Integer.parseInt(stdID.getText());
+        String name = stdName.getText();
+        String major = stdMajor.getText();
+        Double grade = Double.parseDouble(stdGrade.getText());
+        String sql = "Update Student Set name='" + name + "', major='" + 
+                major + "', grade=" + grade + " Where id=" +id;
+        this.statement.executeUpdate(sql);
     }
 
     @FXML
@@ -113,14 +128,24 @@ public class TableViewPaneController implements Initializable {
 
     @FXML
     private void buttonResetHandle(ActionEvent event) {
+        resetControls();
     }
-    private void showSelectedStudent(){
+
+    private void resetControls() {
+        stdID.setText("");
+        stdName.setText("");
+        stdMajor.setText("");
+        stdGrade.setText("");
+        tableView.getItems().clear();
+    }
+
+    private void showSelectedStudent() {
         Student std = tableView.getSelectionModel().getSelectedItem();
-        if(std != null){
-        stdID.setText(String.valueOf(std.getStdId()));
-        stdName.setText(std.getStdName());
-        stdMajor.setText(std.getStdMajor());
-        stdGrade.setText(String.valueOf(std.getStdGrade()));
+        if (std != null) {
+            stdID.setText(String.valueOf(std.getStdId()));
+            stdName.setText(std.getStdName());
+            stdMajor.setText(std.getStdMajor());
+            stdGrade.setText(String.valueOf(std.getStdGrade()));
         }
 
     }
